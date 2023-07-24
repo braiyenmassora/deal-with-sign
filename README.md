030142dcb5f99c58b6aada62f1624355c05e954a
braiyenmassora <braiyenmassora@gmail.com>
2023-07-25 00:37:07 +0700

    fixing hero sections

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# DealWithSign.com
+ DealWithSign.com
 
 DealWithSign.com is a personal portfolio website showcasing my projects built with Next.js and Tailwind CSS. It serves as a platform to exhibit my web development skills, showcase my projects, and provide information about myself as a developer.
 
-## Technologies Used
+ Technologies Used
 
 - Next.js
 - Tailwind CSS
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
 
 const Hero = () => {
-    return (
-        <section className="text-black body-font">
-            <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                    <h1 className="title-font sm:text-9xl text-3xl mb-4 font-medium text-black">Data Engineer </h1>
-                    <p className="text-left sm:text-2xl mt-10 text-black">
-                        I am a systems thinker who enjoys the challenge of solving complex problems.
-                        <br />
-                        Experience working as a Data Engineer and Technical Documentation Engineer
-                        <br />
-                        to describe the functionalities and features of a product
-                        <br />
-                        for the software product development engineering.
-                    </p>
-                </div>
-            </div>
-
-        </section>
-
-    );
-}
+  return (
+    <section className="text-white body-font h-screen flex items-center">
+      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-9xl text-3xl mb-2 font-medium text-white mt-4">
+            Data Engineer
+          </h1>
+          <p className="text-left sm:text-2xl mt-10 text-white">
+            I am a systems thinker who enjoys the challenge of solving complex problems.
+            <br />
+            I love to analyze data and have a passion for building apps using Flutter.
+            <br />
+            You will find me thoroughly enjoying the intricacies.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Hero;
-
-
